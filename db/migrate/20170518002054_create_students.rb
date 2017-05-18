096e7ecaace5c8fd18cf57dfd33b9ed1c0562f1d
Amed <amed157@hotmail.com>
2017-05-18 18:16:29 -0500

    model_student

---
+++ db/migrate/20170518002054_create_students.rb
@@ -0,0 +1,20 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :father_last_name 
+      t.string :mother_last_name
+      t.integer :enrollment
+      t.string :curp
+      t.string :email
+      t.string :address
+      t.integer :cp
+      t.integer :phone, limit: 8
+      t.integer :cell_phone, limit: 8
+      t.string :link_facebook
+      t.string :link_instagram
+      t.string :link_twitter
+      t.timestamps null: false
+    end
+  end
+end
