b9bd54778aaace157121616834fd5059d49d11d6
Amed <amed157@hotmail.com>
2017-06-16 19:52:47 -0500

    migrate to add username to users and install cancancan

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,25 +4,47 @@ ActiveAdmin.register User do
   index do
     selectable_column
     id_column
+    column :first_name
+    column :last_name
+    column :username
+    column :admin
     column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
     actions
   end
 
+  filter :first_name
+  filter :last_name
+  filter :admin
   filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
   filter :created_at
 
   form do |f|
     f.inputs "Admin Details" do
+      f.input :first_name
+      f.input :last_name
+      f.input :username
+      f.input :admin
       f.input :email
       f.input :password
       f.input :password_confirmation
     end
     f.actions
+  end
+
+  show do
+    attributes_table do
+      row :first_name
+      row :last_name
+      row :username
+      row :admin
+      row :email
+      row :encrypted_password
+      row :last_sign_in_at
+      row :created_at
+      row :updated_at
+      row :current_sign_in_ip
+      row :last_sign_in_ip
+    end
   end
 
 end
